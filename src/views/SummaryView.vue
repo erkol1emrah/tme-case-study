5b48a8ef4298fd86f8231db854cfb5788d826efd
erkol1emrah <erkol1emrah@gmail.com>
2022-06-29 04:55:26 +0300

    Summary view completed. (Non-responsive)

--- src/views/SummaryView.vue
+++ src/views/SummaryView.vue
@@ -1,11 +1,117 @@
 <template>
-	<div class="summary">SUMMARY</div>
+	<div class="summary">
+		<div class="car">
+			<div class="circle"></div>
+			<div class="name">{{ cars[store.car].model }}</div>
+			<div class="photo">
+				<img :src="`/images/${cars[store.car].image}`" />
+			</div>
+		</div>
+		<div class="details">
+			<p class="title">MODEL</p>
+			<p class="value">{{ cars[store.car].name }}</p>
+			<hr />
+			<p class="title">COLOR</p>
+			<p class="value">{{ cars[store.car].colors[store.color].name }}</p>
+			<hr v-if="store.packs.length > 0" />
+			<p class="title" v-if="store.packs.length > 0">ACCESSORIES</p>
+			<p class="value" v-for="pack in store.packs">{{ cars[store.car].packs[pack].name }}</p>
+		</div>
+	</div>
 </template>
 
-<style>
+<script>
+import cars from '../data/cars';
+import { store } from '../store';
+
+export default {
+	name: 'Car',
+	props: ['id'],
+	data() {
+		return {
+			store,
+			cars: cars,
+		};
+	},
+};
+</script>
+
+<style scoped>
 .summary {
 	height: 604px;
 	align-items: center;
 	justify-content: center;
+}
+
+.car {
+	position: relative;
+	width: 550px;
+	height: 467px;
+	margin-right: 25px;
+}
+
+.circle {
+	position: absolute;
+	width: 456px;
+	height: 467px;
+	width: 456px;
+	border: 0.9px solid var(--black);
+	border-radius: 50%;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	opacity: 0.06;
+}
+
+.name {
+	position: absolute;
+	font-family: 'Qanelas Extra Bold';
+	font-size: 173.7px;
+	color: var(--black);
+	line-height: 218px;
+	letter-spacing: -8.1px;
+	opacity: 0.09;
+	top: 36px;
+	left: 50%;
+	transform: translate(-50%, 0);
+	z-index: -1;
+}
+
+.photo {
+	position: absolute;
+	height: 250px;
+	width: 550px;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -42%);
+	user-select: none;
+}
+
+.details {
+	flex-flow: column nowrap;
+	width: 324px;
+}
+
+.title {
+	font-family: 'Qanelas Bold';
+	font-size: 12px;
+	color: var(--black);
+	line-height: 15px;
+	margin-bottom: 19px;
+}
+
+.value {
+	font-family: 'Qanelas Medium';
+	font-size: 16px;
+	color: var(--black);
+	line-height: 19px;
+	opacity: 0.38;
+}
+
+hr {
+	margin-top: 32px;
+	margin-bottom: 33px;
+	opacity: 0.09;
+	border: 1px solid #979797;
 }
 </style>
